20cd460d0bc6d00f078c6fbed34406b016bfd376
ajrilatt <ajrilatt@gmail.com>
2023-04-28 22:07:58 -0400

    instructor grade report frontend

--- lib/pages/instructor/grade_report/grade_report.dart
+++ lib/pages/instructor/grade_report/grade_report.dart
@@ -1,29 +1,74 @@
-// Adam Rilatt
-// 24 April 2023
-// Software Architecture -- Project 3 InstructorGradeReport
-
 import 'package:flutter/material.dart';
 
 class InstructorGradeReport extends StatelessWidget {
   const InstructorGradeReport({super.key});
+  static const String _title = 'Assignment Grade Reports';
+  static const List<String> _studentNames = <String>[
+    'Alice',
+    'Bobby',
+    'Charlie',
+    'Doug',
+    'Eve',
+  ];
+  static const List<String> _assignmentNames = <String>[
+    'Hello World!',
+    'Add Two Numbers',
+    'Beekeeper',
+    'Volim',
+    'Steal Tyler\'s Internship',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Instructor Grade Report'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Goober button activate'),
-          onPressed: () {
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => const SubmissionPage()),
-            // );
-          },
-        ),
+      appBar: AppBar(title: const Text(_title)),
+      body: const GradeReportStatefulWidget(
+        studentNames: _studentNames,
+        assignmentNames: _assignmentNames,
       ),
     );
+  }
+}
+
+class GradeReportStatefulWidget extends StatefulWidget {
+  const GradeReportStatefulWidget({
+    super.key,
+    required this.studentNames,
+    required this.assignmentNames,
+  });
+
+  final List<String> studentNames;
+  final List<String> assignmentNames;
+
+  @override
+  State<GradeReportStatefulWidget> createState() => _GradeReportState();
+}
+
+class _GradeReportState extends State<GradeReportStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Drawer(
+            child: ListView.separated(
+      padding: const EdgeInsets.all(12.0),
+      itemCount: widget.assignmentNames.length,
+      itemBuilder: (_, int aIndex) {
+        return ExpansionTile(
+          title: Text(widget.assignmentNames[aIndex]),
+          subtitle: const Text('avg: x\tmin: y\tmax:  z'),
+          children: <Widget>[
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: widget.studentNames.length,
+              itemBuilder: (_, int sIndex) {
+                return ListTile(title: Text(widget.studentNames[sIndex]));
+              },
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    )));
   }
 }
